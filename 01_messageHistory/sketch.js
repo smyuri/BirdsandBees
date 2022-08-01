33d5aad876c22ab0c0a7085d56d92c6295a11d00
smyuri <105171669+smyuri@users.noreply.github.com>
2022-07-31 21:44:27 -0400

    Update sketch.js

--- 01_messageHistory/sketch.js
+++ 01_messageHistory/sketch.js
@@ -4,8 +4,7 @@ let subKey = "sub-c-5c7c93ad-42b7-4af3-9cce-e78ea25ba5c2";
 let secretKey = "sec-c-ZmZjMTE2NWEtNzE3OS00ZWUwLWFiOGMtN2NlNDlkZDRkYmIy";
 
 let channelName = "history";
-let bg;
-let img;
+
 let you;
 
 //input variables for the form to PubNub
@@ -16,8 +15,6 @@ let history;
 
 function preload() { 
 
-bg= loadImage ("IMG_2605.PNG")
-img= loadImage("IMG_2606.PNG")
   // logic to create a random UUID
     you = random(0,1000000); 
     console.log(you);
@@ -31,8 +28,7 @@ img= loadImage("IMG_2606.PNG")
 function setup() {
 
     createCanvas(windowWidth, windowHeight);
-image(img, 0, 0, img.width/3, img.height/3);
-image(bg, 25, 25, img.width/3, img.height/3);
+
     dataServer = new PubNub({
       subscribeKey: subKey,
       publishKey: pubKey,
@@ -52,8 +48,8 @@ image(bg, 25, 25, img.width/3, img.height/3);
     //create the text fields for the message to be sent
     sendText = createInput();
     sendText.position((windowWidth/2) - 100, windowHeight *0.8);
-  sendText.fontcolor(255)
-    sendButton = createButton("SEND");
+  
+    sendButton = createButton("Send a message");
     sendButton.position(sendText.x + sendText.width, windowHeight * 0.8);
     sendButton.mousePressed(sendTheMessage);
 
@@ -62,7 +58,7 @@ image(bg, 25, 25, img.width/3, img.height/3);
 }
   
 function draw() {
-background(0)
+ 
 
 
 }
@@ -90,12 +86,11 @@ function drawMessages(messageHistory) {
   console.log("in draw messages");
 
   console.log(messageHistory);
-  textColor(255);
   textSize(80);
   for (let i = 0; i < messageHistory.length; i++) {
     
-      console.log(messageHistory[i]);
-      text(messageHistory[i].message.messageText, windowWidth/2, 100 * (i+1));
+      console.log(messageHistory[50]);
+      text(messageHistory[50].message.messageText, windowWidth/2, 100 * (i+1));
 
   }
 
